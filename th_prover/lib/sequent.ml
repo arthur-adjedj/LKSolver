22fbcc30dfe3b57fecca6823dedc401355275df0
BuildTools <unconfigured@null.spigotmc.org>
2021-05-08 16:46:03 +0200

    Ajout de print_sequent + correction mineure

--- th_prover/lib/sequent.ml
+++ th_prover/lib/sequent.ml
@@ -8,7 +8,7 @@ type sequent = (formule array)* (formule array)
 
 (*On suppose lf1 et lf2 triées par Array.fast_sort*)
 let eq_sequents (gf1,df1) (gf2,df2) :bool =    
-    if (Array.for_all2 eq_formule gf1 gf2) && (Array.for_all2 eq_formule df1 df2)
+    if not ((Array.for_all2 eq_formule gf1 gf2) && (Array.for_all2 eq_formule df1 df2))
       then raise (Unequal_length  false)
   else begin 
     let dict = Hashtbl.create 64 in
@@ -22,6 +22,24 @@ let eq_sequents (gf1,df1) (gf2,df2) :bool =
         |Imp(a1,b1),Imp(a2,b2) -> (aux a1 a2) && (aux b1 b2)
         |_ -> false
     in (Array.for_all2 aux gf1 gf2) && (Array.for_all2 aux df1 df2) end;;
+
+
+let print_sequent (gf,df) =
+  let n1 = Array.length gf
+  and n2 = Array.length df in 
+  for i=0 to n1-1 do 
+    print_char '(';
+    print_formule gf.(i);
+    print_char ')';
+    if i<>(n1-1) then print_char ','
+  done;
+  print_string "-|";
+  for i=0 to n2-1 do 
+    print_char '(';
+    print_formule df.(i);
+    print_char ')';
+    if i<>(n2-1) then print_char ','
+  done;
 
 
 
