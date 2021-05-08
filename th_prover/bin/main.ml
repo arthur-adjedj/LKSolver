60817d52270e835e9f104b4a0715cddf1fb4225a
BuildTools <unconfigured@null.spigotmc.org>
2021-05-08 16:48:45 +0200

    modifications inutiles

--- th_prover/bin/main.ml
+++ th_prover/bin/main.ml
@@ -1,12 +1,18 @@
 open Mylib.Formule
+open Mylib.Sequent
+open Mylib.Tactiques
 
 let print_bool = function
 	|true -> (print_endline "true")
 	|false -> (print_endline "false")
 
 
-let () = print_bool (eq_formule (And(Not (Var 'a'),Var 'b')) (And(Not(Bottom), Or((Var 'a'),Var 'b'))));
-				print_bool (eq_formule (And(Not (Var 'a'),Var 'a')) (And(Not(Bottom), Or((Var 'a'),Var 'b'))))
+let () = 	print_sequent ([||],[|Imp(Var 'a',Var 'b')|]); print_endline "";
+					print_hypos (imp_droite 0 ([||],[|Imp(Var 'a',Var 'b')|])); print_endline "";
+					print_formule (Imp(Imp(Not (Var 'b'),Not (Var 'a')),Imp(Var 'a',Var 'b')));print_endline "";
+					print_sequent ( [|(Imp (Not(Var 'b') , Not(Var 'a')))|] , [| Imp(Var 'a',Var 'b') |] );print_endline "";
+
+
 
 
 
