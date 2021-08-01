141b3e7d2c3d9a2c7f4c3d557ab6e45f84974026
arthur-adjedj <aarthuur01@gmail.com>
2021-08-01 14:30:04 +0200

    made the symetry rule into one unique rune

--- pr_assistant/proof_build/nat_tacts.ml
+++ pr_assistant/proof_build/nat_tacts.ml
@@ -4,24 +4,21 @@ open Formule
 open Nat
 
 let refl s =
-  if is_s_equiv ([||],[|Nat_comp (Equal(Var_nat 'a',Var_nat 'b'))|])  s 
+  if is_s_equiv ([||],[|Nat_comp (Equal(Var_nat 'a',Var_nat 'a'))|])  s 
     then ([],"refl")
   else 
     raise (Wrong_sequent ([s],"wrong_sequent, refl"))
   
-let syml s i = 
-  let gres = Array.copy (fst s)
-  and dres = Array.copy (snd s) in 
-  match (fst s).(i) with
-    |Nat_comp (Equal(a,b)) -> gres.(i) <- (Nat_comp(Equal(b,a)));
-   (gres,dres)
 
-let symr s i = 
+let sym c s i = 
   let gres = Array.copy (fst s)
   and dres = Array.copy (snd s) in 
-  match (snd s).(i) with
-    |Nat_comp (Equal(a,b)) -> dres.(i) <- (Nat_comp(Equal(b,a)));
+  if c= 'l' then match (fst s).(i) with
+    |Nat_comp (Equal(a,b)) -> gres.(i) <- (Nat_comp(Equal(b,a)));
+     ((gres,dres),"syml")
+    |_ -> raise (Wrong_sequent ([s],"wrong_sequent syml"));
+else match (snd s).(i) with
+   |Nat_comp (Equal(a,b)) -> dres.(i) <- (Nat_comp(Equal(b,a)));
    ((gres,dres),"symr")
    |_ -> raise (Wrong_sequent ([s],"wrong_sequent symr"))
-
 
